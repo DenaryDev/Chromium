ea47abfc17f13acf97c16a82583e4fd20e600364
DenaryDev <denaryplanet@gmail.com>
2022-02-26 20:31:44 +0500

    Add license header for MixinMinecraftClient.java

--- src/main/java/io/sapphiremc/chromium/mixin/client/MixinMinecraftClient.java
+++ src/main/java/io/sapphiremc/chromium/mixin/client/MixinMinecraftClient.java
@@ -1,3 +1,10 @@
+/*
+ * Copyright (c) 2022 DenaryDev
+ *
+ * Use of this source code is governed by an MIT-style
+ * license that can be found in the LICENSE file or at
+ * https://opensource.org/licenses/MIT.
+ */
 package io.sapphiremc.chromium.mixin.client;
 
 import net.minecraft.client.MinecraftClient;
